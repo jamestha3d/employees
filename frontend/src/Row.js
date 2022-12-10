ca6770c67d90184b138d1e0a85f1c6f6a38cbf2c
jamestha3d <jamesikediashi@gmail.com>
2022-12-10 02:09:26 -0500

    psycopg2

--- frontend/src/Row.js
+++ frontend/src/Row.js
@@ -1,6 +1,17 @@
 const Row = ({employee, index, deleteEmployee, edit}) => {
+    const onMouseOver = () => {
+        
+        //document.querySelector(`#editField${index}`).style.display = "block";
+        
+    }
+
+    const mouseLeft = () => {
+        
+        //document.querySelector(`#editField${index}`).style.display = "none";
+    }
     return ( 
-        <tr key={index}>
+        <>
+        <tr className="tableRow" onMouseEnter={onMouseOver} onMouseLeave={mouseLeft} key={index}>
             <td> {employee.gender}</td>
             <td> {employee.title}</td>
             <td> {employee.givenName}</td>
@@ -10,9 +21,10 @@ const Row = ({employee, index, deleteEmployee, edit}) => {
             <td> {employee.state}</td>
             <td> {employee.zipcode}</td>
             <td> {employee.birthday}</td>
-            <td> <button onClick={() => edit(employee)} className='btn btn-sm btn-outline-info'> Edit </button> <button onClick={() => deleteEmployee(employee)} className='btn btn-sm btn-outline-danger'> Del </button></td>
+            <td data-info={index} id={`editField2${index}`} className="editField" > <button onClick={() => edit(employee)} className='btn btn-sm btn-outline-info'> Edit </button> <button onClick={() => deleteEmployee(employee)} className='btn btn-sm btn-outline-danger'> Del </button></td>
         </tr>
-
+        
+        </>
     );
 }
  
